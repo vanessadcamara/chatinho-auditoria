a81c526751779f6eb957f26a263d535b5d24613f
Anna Milani <aavm.snf19@uea.edu.br>
2022-03-14 20:48:34 -0400

    comentando codigo

--- client.py
+++ client.py
@@ -1,41 +1,54 @@
-from ctypes import sizeof
+# importações
 from Cryptodome.Cipher import AES
 from Cryptodome.Util import Padding
 from cryptography.fernet import Fernet
 import socket
 
+# inserção da senha para conexão
 senha = ''
-while(len(senha) != 16):
+while(len(senha) not in (16, 24, 32)):
     senha = input("Insira a senha: ")
 
+# criação da chave inicial
 chave_inicial = senha.encode()
+
+# criação da IV, que possui o mesmo tamanho da chave inicial
 IV = b"A" * len(chave_inicial)
 
-def decrypt(cipher):
-    decryptor = AES.new(chave_inicial, AES.MODE_CBC, IV)
-    decrypted_padded_message = decryptor.decrypt(cipher)
-    decrypted_message = Padding.unpad(decrypted_padded_message, 16)
-    return decrypted_message
-
+def descriptografar(mensagem):
+    descriptografador = AES.new(chave_inicial, AES.MODE_CBC, IV)
+    decrypted_padded_message = descriptografador.decrypt(mensagem)
+    mensagem_descriptografada = Padding.unpad(decrypted_padded_message, 16)
+    return mensagem_descriptografada
+    
+# inicialização do socket
 s = socket.socket()
-host = input(str('Enter hostname or host IP: '))
-port = 8080
-s.connect((host, port))
-print('Connected to chat server')
 
+# captura do host
+host = input('Entre com hostname: ')
+port = 8080
+
+# Vincula o socket ao endereço
+s.connect((host, port))
+print('Conectado com o servidor do chat')
+
+# Envio da confirmação da criptografia
 chave_mensagens = s.recv(1024)
-print("Chave recebida criptografada: ", chave_mensagens)
-chave_mensagens = decrypt(chave_mensagens)
-print("Chave recebida descriptografada: ", chave_mensagens)
+# print("Chave recebida criptografada: ", chave_mensagens)
+
+# Descriptografar a chave das mensagens
+chave_mensagens = descriptografar(chave_mensagens)
+# print("Chave recebida descriptografada: ", chave_mensagens)
 funcao_cripto_mensagens = Fernet(chave_mensagens)
+
+# Envio da confirmação da criptografia
 s.send(funcao_cripto_mensagens.encrypt(b'criptografia iniciada'))
 
 while 1:
     incoming_message = s.recv(1024)
-    print("Mensagem recebida: ", incoming_message)
+    # print("Mensagem recebida: ", incoming_message)
     incoming_message = funcao_cripto_mensagens.decrypt(incoming_message).decode()
-    print('Server : ', incoming_message, "\n")
-    message = input(str('>> '))
-    message = message.encode()
-    s.send(funcao_cripto_mensagens.encrypt(message))
-    print('Sent\n')
+    print('Servidor >> ', incoming_message)
+    mensagem = input('Cliente >> ')
+    mensagem = mensagem.encode()
+    s.send(funcao_cripto_mensagens.encrypt(mensagem))
--- server.py
+++ server.py
@@ -1,51 +1,68 @@
-import socket
-
+# importações
 from Cryptodome.Cipher import AES
 from Cryptodome.Util import Padding
 from cryptography.fernet import Fernet
 import socket
 
+# criação da senha
 senha = ''
 while(len(senha) not in (16, 24, 32)):
     senha = input("Crie uma senha de 16, 24 ou 32 caracteres: ")
+    
 # criação da chave inicial
 chave_inicial = senha.encode()
-#criação da IV, que possui o mesmo tamanho da chave inicial
+
+# criação da IV, que possui o mesmo tamanho da chave inicial
 IV = b'A' * len(chave_inicial) 
+
 # geração da chave que será utilizada nas mensagens
 chave_mensagens = Fernet.generate_key()
-print("Chave do fernet:", chave_mensagens)
-# criação da função que garante que uma mensagem encriptografada não pode ser manipulada ou lida sem a chave.
+# print("Chave do fernet:", chave_mensagens)
+
+# criação da função que garante que uma mensagem encriptografada 
+# não pode ser manipulada ou lida sem a chave
 funcao_cripto_mensagens = Fernet(chave_mensagens)
 
-def encrypt(message):
+def encriptografar(mensagem):
     encryptor = AES.new(chave_inicial, AES.MODE_CBC, IV)
-    padded_message = Padding.pad(message, 16)
-    encrypted_message = encryptor.encrypt(padded_message)
-    return encrypted_message
+    padded_mensagem = Padding.pad(mensagem, 16)
+    mensagem_encriptografada = encryptor.encrypt(padded_mensagem)
+    return mensagem_encriptografada
 
+# inicialização do socket
 s = socket.socket()
-host = socket.gethostname()
-print(' Server will start on host : ', host)
+
+# captura do host
+host = socket.gethostname() 
+print('Servidor vai começar no host: ', host)
 port = 8080
+
+# Vincula o socket ao endereço
 s.bind((host, port))
-print('Waiting for connection\n')
+print('Esperando conexão...\n')
+
+# Quantidade de conexões do servidor
 s.listen(1)
 conn, addr = s.accept()
-print(addr, 'Has connected to the server\n')
-conn.send(encrypt(chave_mensagens))
-incoming_message = conn.recv(1024)
-print("Mensagem recebida: ", incoming_message)
-incoming_message = funcao_cripto_mensagens.decrypt(incoming_message).decode()
-print(' Client : ', incoming_message)
+print(addr, 'conectou ao servidor.\n')
+
+# Envio da chave criptografada
+conn.send(encriptografar(chave_mensagens))
+
+# Recebimento da confirmação da criptografia
+mensagem_recebida = conn.recv(1024)
+# print("Mensagem recebida: ", mensagem_recebida)
+
+# Descriptografar a mensagem recebida
+mensagem_recebida = funcao_cripto_mensagens.decrypt(mensagem_recebida).decode()
+print('Cliente >> ', mensagem_recebida)
 
 while 1:
-    message = input(str('>> '))
-    message = message.encode()
-    conn.send(funcao_cripto_mensagens.encrypt(message))
-    print('Sent\n')
-    incoming_message = conn.recv(1024)
-    print("Mensagem recebida: ", incoming_message)
-    incoming_message = funcao_cripto_mensagens.decrypt(incoming_message).decode()
-    print(' Client: ', incoming_message, "\n")
+    mensagem = input('Servidor >> ')
+    mensagem = mensagem.encode() 
+    conn.send(funcao_cripto_mensagens.encrypt(mensagem))
+    mensagem_recebida = conn.recv(1024)
+    # print("Mensagem recebida: ", mensagem_recebida)
+    mensagem_recebida = funcao_cripto_mensagens.decrypt(mensagem_recebida).decode()
+    print('Cliente >> ', mensagem_recebida)
     
